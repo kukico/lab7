d14794387f451ef83543d569519ae71d4a085e00
kukico <shanxipcs@aliyun.com>
2022-11-22 10:41:02 +0000

    1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         // Instantiate an object of class Calculator with initial value of zero
-        System.out.println("Hello World!");
+        System.out.println("Good Morning");
     }
 }
 
